cbbe468388be45166789cb9cbba64897d9b43e78
Steve Landry Tene <steve.tene@gmail.com>
2021-11-23 22:21:50 -0500

    adding billing service logic

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,6 +1,8 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
 import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
@@ -15,6 +17,12 @@ class BillingService(
                 invoice.status = InvoiceStatus.PAID
             }
         } catch (e: CustomerNotFoundException) {
+            // TODO error logging
+        } catch (e: CurrencyMismatchException) {
+            // TODO error logging
+        } catch (e: NetworkException) {
+            // TODO error logging
+        } catch (e: Exception) {
             // TODO error logging
         }
         return invoice
